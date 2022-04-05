450e359eb93fd32c3c9ec4e4a9dc60a8fb1d8cae
Ariel Zingman <arielzingman@gmail.com>
2022-04-05 17:20:20 +0300

    wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,13 +6,14 @@ const forecast = (long, lat, callback) =>{
     const url = 'http://api.weatherstack.com/current?access_key=b02c847e63d3de575106f932fb52e5c1&query=' + long + ',' + lat
 
     request({url, json: true}, (error, {body}) => {
-
+        // console.log(body)
         if (error) {
             callback('cannot connect to the weather api')
         } else if (body.error) {
             callback('unable to find location')
         } else {
-            callback(undefined, 'degrees: ' + body.current.temperature + ' feels like: ' + body.current.feelslike)
+            callback(undefined, 'degrees: ' + body.current.temperature + ' feels like: ' + body.current.feelslike + ' wind speed is: ' + body.current.wind_speed)
+    
         }
     })
 }
